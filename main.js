5fa74e49898c3be8e150d068b80076a9ad8e04fb
plabhesh98 <81747071+plabhesh98@users.noreply.github.com>
2022-01-16 12:29:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    Clown_nose = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet is Intialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y - 10;
+        console.log("Nose x = " + noseX);
+        console.log("Nose y = " + noseY);
+    }
+}
+
+function draw(){
+    image(video, 0,0,300,300);
+    image(Clown_nose,noseX,noseY,30,30);
+}
+
+function take_snapshot(){
+    save("Clown_Nose_image.jpeg");
+}
